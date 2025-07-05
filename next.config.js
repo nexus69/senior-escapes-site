fdd076367b49b2046abe7b403a50b6659034882e
nexus69 <nexus69@users.noreply.github.com>
2025-07-05 20:14:40 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  assetPrefix: isProd ? '/senior-escapes-site/' : '',
-  basePath: isProd ? '/senior-escapes-site' : '',
+  assetPrefix: '/senior-escapes-site/',
+  basePath: '/senior-escapes-site',
 };
 
 module.exports = nextConfig;
