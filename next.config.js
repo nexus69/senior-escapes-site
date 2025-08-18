1c1052c4a54e07b05dd23e3d71211f3870352df4
nexus69 <nexus69@users.noreply.github.com>
2025-08-18 13:56:50 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Next 13/14 : export statique
   output: 'export',
   trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
+  images: { unoptimized: true },
+
+  // Désactive ESLint pendant le build Vercel
+  eslint: { ignoreDuringBuilds: true },
 };
 
 module.exports = nextConfig;
